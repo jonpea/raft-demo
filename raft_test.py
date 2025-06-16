85feff01ec0dc8af972aebfd53a7682366eafc58
Jon Pearce <jonpea@gmail.com>
2025-06-16 11:03:53 -0400

    WIP

--- raft_test.py
+++ raft_test.py
@@ -1,3 +1,5 @@
+import collections
+import operator
 from typing import Callable, Final
 from hypothesis import Phase, Verbosity, seed, settings
 from hypothesis.stateful import RuleBasedStateMachine, invariant, rule
@@ -11,7 +13,7 @@ LOG_VALUES: Final = [111, 222, 333]
 
 @seed(0)
 @settings(
-    max_examples=5000,
+    max_examples=1000,
     derandomize=False,
     verbosity=Verbosity.normal,
     phases=tuple(Phase),
@@ -21,13 +23,14 @@ LOG_VALUES: Final = [111, 222, 333]
 class RaftStateMachine(RuleBasedStateMachine):
     def __init__(self):
         super().__init__()
-        self.system = raft.System.init(serverIds=SERVER_IDS)
+        self.sys = self.sysprev = raft.System.init(serverIds=SERVER_IDS)
         self.data = raft.SystemData.init(serverIds=SERVER_IDS, logValues=LOG_VALUES)
 
     def step_through(self, transition, *args):
         transit: Callable[..., raft.System | None] = transition(*args)
-        system: raft.System | None = transit(self.system)
-        self.system = self.data.step(system or self.system)
+        self.sysprev = self.sys
+        sysnext: raft.System | None = transit(self.sys)
+        self.sys = self.data.step(sysnext or self.sys)
 
     @rule(
         serverId=st.sampled_from(SERVER_IDS),
@@ -48,20 +51,46 @@ class RaftStateMachine(RuleBasedStateMachine):
         self.step_through(raft.ClientRequest, serverId, logValue)
 
     @invariant()
-    def single_leader_per_term(self):
+    def unique_leader(self):
+        """In each term, one server is designated leader"""
         leaders_by_term: dict[raft.TermId, raft.ServerId] = {}
-        for i in self.system.serverIds:
-            if self.system.state[i] == raft.ServerState.LEADER:
-                term = self.system.currentTerm[i]
-                assert term not in leaders_by_term, f"Multiple leaders in term {term}"
+        for i in self.sys.serverIds:
+            if self.sys.state[i] == raft.ServerState.LEADER:
+                term = self.sys.currentTerm[i]
+                assert term not in leaders_by_term, "uniqueness"
                 leaders_by_term[term] = i
 
     @invariant()
     def log_consistency(self):
-        for i in self.system.serverIds:
-            log: list[raft.LogEntry] = self.system.log[i]
-            for entry in log:
-                assert entry.term <= self.system.currentTerm[i], "Invalid term in log"
+        """Log terms increase in step with term counter."""
+        for i in self.sys.serverIds:
+            currentTerm = self.sys.currentTerm[i]
+            logTerms = [entry.term for entry in self.sys.log[i]]
+            assert max(logTerms, default=0) <= currentTerm, "causal"
+            assert all(map(operator.le, logTerms[:-1], logTerms[1:])), "non-decreasing"
+
+    @invariant()
+    def leader_logs(self):
+        """The leader preserves it own logs."""
+        for i in self.sys.serverIds:
+            if self.sys.state[i] is raft.ServerState.LEADER:
+                if self.sysprev.state[i] is not raft.ServerState.LEADER:
+                    assert all(  # maps over the shorter sequence
+                        map(operator.eq, self.sysprev.log[i], self.sys.log[i])
+                    )
+
+    @invariant()
+    def term_counter(self):
+        """Term counter."""
+        for i in self.sys.serverIds:
+            assert (
+                self.sysprev.currentTerm[i] <= self.sys.currentTerm[i]
+            ), "non-decreasing"
+
+    @invariant()
+    def one_vote_per_server(self):
+        # counts = collections.Counter(self.sys.elections)
+        pass
 
 
 # Works with pytest or unittest
